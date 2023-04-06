7ead0c1176cecf2dfbe2806bd95e03dfb4fff308
= <prathameshkulkarni710@gmail.com>
2023-04-06 13:33:59 +0530

    Loader Component Improved

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,14 +1,28 @@
-import { Box } from '@mui/material'
-import React, { useContext } from 'react'
-import Context from '../context'
+import { Box } from "@mui/material";
+import React, { useContext } from "react";
+import Context from "../context";
 
 const Loader = () => {
-    const {loading}=useContext(Context)
-  return loading?(
-    <Box sx={{position:'absolute',top:"70px",margin:"0px auto",backgroundColor:'red',left:'50%',padding:'5px 30px',color:'white',fontWeight:'550',borderRadius:"4px"}}>
-     Loading...   
-    </Box>
-  ):(<></>)
-}
+  const { loading } = useContext(Context);
+  return (
+    loading && (
+      <Box
+        sx={{
+          position: "absolute",
+          top: "70px",
+          margin: "0px auto",
+          backgroundColor: "red",
+          left: "50%",
+          padding: "5px 30px",
+          color: "white",
+          fontWeight: "550",
+          borderRadius: "4px",
+        }}
+      >
+        Loading...
+      </Box>
+    )
+  );
+};
 
-export default Loader
+export default Loader;
